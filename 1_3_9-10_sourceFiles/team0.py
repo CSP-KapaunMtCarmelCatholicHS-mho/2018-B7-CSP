b8b084da1acb77a6444ae470b56e94c3055bf9cc
reutiteuti <rzach@outlook.com>
2019-03-18 12:00:04 -0500

    Add my strategy
    Added my strategy to my team file

--- 1_3_9-10_sourceFiles/team0.py
+++ 1_3_9-10_sourceFiles/team0.py
@@ -6,27 +6,25 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Reuben' # Only 10 chars displayed.
+strategy_name = 'Collude'
+strategy_description = 'Always Collude'
+    
     
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
+    '''Make my move based on the history with this player.
     
-    Make my move.
-    Returns 'c' or 'b'. 
+    history: a string with one letter (c or b) per round that has been played with this opponent.
+    their_history: a string of the same length as history, possibly empty. 
+    The first round between these two players is my_history[0] and their_history[0]
+    The most recent round is my_history[-1] and their_history[-1]
+    
+    Returns 'c' or 'b' for collude or betray.
     '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
     
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
+    # This player always colludes.
     return 'c'
+    
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
