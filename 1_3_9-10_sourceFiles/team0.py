2535fb7dfde5e54d3b8c5d877098e43314fc73e7
Reuben Scheck <schreu@CRUSADER.KMC>
2019-03-20 12:55:32 -0500

    change strategy

--- 1_3_9-10_sourceFiles/team0.py
+++ 1_3_9-10_sourceFiles/team0.py
@@ -7,28 +7,27 @@
 ####
 
 team_name = 'Reuben' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+strategy_name = 'Pure Genius'
+strategy_description = 'Look at probability'
     
+def prob_of_b(their_history):
+    if their_history.count('b') > len(their_history)/2:
+        return 'b'
+    else:
+        return 'c'
+
+def compare_scores(my_score, their_score):
+    if their_score > my_score:
+        return 'b'
+    else:
+        return 'c'
+      
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
-    
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
-
-    
+    if prob_of_b() == compare_scores():
+        prob_of_b()
+    else:
+        return 'b'
+        
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
     from this module. Prints error if return value != result.
