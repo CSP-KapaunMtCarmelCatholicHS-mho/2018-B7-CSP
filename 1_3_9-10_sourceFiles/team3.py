a1de9dc931fc93875b5f5775ca4be350695b1249
Johnny Dao <43384292+alpha2234@users.noreply.github.com>
2019-03-22 12:32:17 -0500

    Johnny Dao
    Team-3

--- 1_3_9-10_sourceFiles/team3.py
+++ 1_3_9-10_sourceFiles/team3.py
@@ -7,8 +7,8 @@
 ####
 
 team_name = 'Team 3' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+strategy_name = 'strategy'
+strategy_description = 'just does it'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
