23f0d1c2adf885d1f9e5693ebc343660e6480603
cricket171 <43384205+cricket171@users.noreply.github.com>
2019-03-22 12:34:23 -0500

    Changed Team to 11

--- 1_3_9-10_sourceFiles/team11.py
+++ 1_3_9-10_sourceFiles/team11.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Luke Billinger' # Only 10 chars displayed.
+strategy_name = 'Best strat'
+strategy_description = 'Victory'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
