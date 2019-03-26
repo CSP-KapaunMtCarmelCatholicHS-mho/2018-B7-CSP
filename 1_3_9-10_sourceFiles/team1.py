9b5252573d16f43a13bc5dbef2345d50e80ca8df
MrMilk2046 <43384956+MrMilk2046@users.noreply.github.com>
2019-03-26 12:16:14 -0500

    Revert "test_move"
    This reverts commit ca1f46fbcf8e187acbf6a372b5d9dcc5710f804d.

--- 1_3_9-10_sourceFiles/team1.py
+++ 1_3_9-10_sourceFiles/team1.py
@@ -12,23 +12,3 @@ strategy_description = 'always colludes'
     
 def move(my_history, their_history, my_score, their_score):
     return 'c'
-
-def test_move(my_history, their_history, my_score, their_score, result):
-    '''calls move(my_history, their_history, my_score, their_score)
-    from this module. Prints error if return value != result.
-    Returns True or False, dpending on whether result was as expected.
-    '''
-    real_result = move(my_history, their_history, my_score, their_score)
-    if real_result == result:
-        return True
-    else:
-        print("move(" +
-            ", ".join(["'"+my_history+"'", "'"+their_history+"'",
-                       str(my_score), str(their_score)])+
-            ") returned " + "'" + real_result + "'" +
-            " and should have returned '" + result + "'")
-        return False
-    
-# Test 1
-if test_move(my_history='', their_history='', my_score=0, their_score=0, result='c'):
-    print 'Test passed'
