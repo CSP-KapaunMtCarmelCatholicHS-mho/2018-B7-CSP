e67f9975ce571accf6bb46fa86037b2ed7ac9d69
tbernass <tbernas@cox.net>
2019-03-26 12:13:28 -0500

    Update team10.py
    team name update

--- 1_3_9-10_sourceFiles/team10.py
+++ 1_3_9-10_sourceFiles/team10.py
@@ -6,7 +6,7 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'Team 10' # Only 10 chars displayed.
+team_name = 'Team Burn us' # Only 10 chars displayed.
 strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
     
