7f25a25649e68d67f6e727444e35cc6ec53c3776
emmebina <43384055+emmebina@users.noreply.github.com>
2019-03-28 12:06:56 -0500

    Developed Move Algorithm
    Colude on first move, then becomes dependent on opponent's previous move.

--- 1_3_9-10_sourceFiles/team6.py
+++ 1_3_9-10_sourceFiles/team6.py
@@ -6,7 +6,7 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'cool' # Only 10 chars displayed.
+team_name = 'M.E.' # Only 10 chars displayed.
 strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
     
@@ -17,6 +17,16 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
+    if len(my_history) == 0:
+        return'c'
+    elif my_history[-1] =='c' and their_history[-1] == 'b':
+        if my_history%3==0:
+            return 'c'
+        else:
+            return 'b'
+    else:
+        return'c'
+    
 
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
@@ -26,7 +36,6 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
@@ -47,13 +56,7 @@ def test_move(my_history, their_history, my_score, their_score, result):
 
 if __name__ == '__main__':
      
-    # Test 1: Betray on first move.
-    if test_move(my_history='',
-              their_history='', 
-              my_score=0,
-              their_score=0,
-              result='b'):
-         print 'Test passed'
+
      # Test 2: Continue betraying if they collude despite being betrayed.
     test_move(my_history='bbb',
               their_history='ccc', 
@@ -65,4 +68,11 @@ if __name__ == '__main__':
               # move('bbb', 'ccc', 0, 0) returns 'b'.
               my_score=0, 
               their_score=0,
-              result='b')             
+              result='b') 
+    #Collude first test
+    if test_move(my_history='',
+              their_history='', 
+              my_score=0,
+              their_score=0,
+              result='c'):
+         print 'Test passed'  
